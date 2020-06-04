d72a6cd08d0ec57bcd64b729e082f937aba74d72
Lynxy <anton@timonenkov.ru>
2020-06-04 21:25:09 +0200

    Initial Commit

---
+++ Skillbox 11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skillbox 11
+//
+//  Created by Антон Тимоненков on 04.06.2020.
+//  Copyright © 2020 Антон Тимоненков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
